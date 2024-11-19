78e7eb7273f323a736cafd30891400bd10343ab9
zaherzeinni <zaherzeinni@hotmail.com>
2024-11-19 18:07:25 +0700

    updatee

--- components/components/footer/Footer.js
+++ components/components/footer/Footer.js
@@ -261,7 +261,7 @@ const Footer = ({ style }) => {
       </div>
 
       <a
-        href="https://wa.me/+11234567890" // Replace with your WhatsApp number
+        href="https://wa.me/+96170707627" // Replace with your WhatsApp number
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-2 rounded-full shadow-lg hover:bg-green-600 transition-colors duration-300 z-50"
