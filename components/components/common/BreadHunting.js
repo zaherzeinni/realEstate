2d6edbe8c6d0d8f43ee5dcee396f8fb83790a4d7
Zaher Zeinni <zaherzeinni@hotmail.com>
2025-01-18 13:41:03 +0200

    okkk

--- components/components/common/BreadHunting.js
+++ components/components/common/BreadHunting.js
@@ -12,7 +12,7 @@ const BreadHunting = ({ pagename, pagetitle }) => {
 
   return (
     <div
-      className="breadcrumb-section  p-7 sm:p-[150px]"
+      className="breadcrumb-section  "
       style={{
         backgroundImage:
           "linear-gradient(270deg, rgba(0, 0, 0, .3), rgba(0, 0, 0, 0.3) 101.02%), url(/images/new/InvestPage.webp)",
